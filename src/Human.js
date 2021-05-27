4c83c760e419fbc7faee61e2b2547ae70c6ba6ec
seita1996 <taha.seita@gmail.com>
2021-05-27 22:04:02 +0900

    アニメーションパターンを変更

--- src/Human.js
+++ src/Human.js
@@ -3,18 +3,26 @@ import { Sprite } from '@inlet/react-pixi'
 function HumanAnimate() {
     // 2枚の絵が交互にアニメーション
     const [x, setX] = useState(0)
+    const [pic, setPic] = useState('01_normal')
     useEffect(() => {
       let xVal = 0
       const interval = setInterval(() => {
-        xVal = xVal == 0 ? 1080 : 0
-        setX(xVal)    
+        xVal = xVal === 0 ? 1080 : 0
+        setX(xVal)
       }, 1000);
-      return () => clearInterval(interval)
+
+      // 4秒おきにアニメーションパターンを変更（試行）
+      let picName = '01_normal'
+      const interval2 = setInterval(() => {
+        picName = picName === '01_normal' ? '02_headtap' : '01_normal'
+        setPic(picName)
+      }, 4000);
+      return () => clearInterval(interval2)
     }, [])
 
     return (
       <Sprite
-        image={ humanImage() }
+        image={ humanImage(pic) }
         scale={{ x: 1, y: 1 }}
         anchor={0.5}
         x={x}
@@ -23,10 +31,7 @@ function HumanAnimate() {
     )
 }
 
-function humanImage() {
-  let picName
-  picName = '01_normal'
-  // picName = '02_headtap'
+function humanImage(picName) {
   return `${process.env.PUBLIC_URL}/img/human/${picName}.png`
 }
 

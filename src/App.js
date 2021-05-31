918613bb26f6177f1a68ed5ba838711b0183b16f
seita1996 <taha.seita@gmail.com>
2021-05-31 23:32:49 +0900

    delete text component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import { Stage } from '@inlet/react-pixi'
-import RotatingText from './RotatingText'
-import MovingText from './MovingText'
 import Human from './Human'
 
 function App() {
   return (
     <Stage width={1080} height={1920} options={{ backgroundColor: 0xeef1f5 }}>
-      <RotatingText />
-      <MovingText />
       <Human />
     </Stage>
   );
